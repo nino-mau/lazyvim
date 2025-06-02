d5b7750d8466ac32fa9e3c0b0bee8e699ba9da05
nino.mauu <nino.mauuary@gmail.com>
2025-06-03 00:04:00 +0200

    Update plugins

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,6 +1,12 @@
 return {
   "saghen/blink.cmp",
   opts = {
+    completion = {
+      ghost_text = {
+        enabled = false,
+      },
+    },
+    signature = { enabled = true },
     keymap = {
       ["<Tab>"] = { "select_next", "accept", "fallback" },
       ["<S-Tab>"] = { "select_prev", "accept", "fallback" },
